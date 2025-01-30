05dde162b0a62eef44ee6f90c44b0c89e751deda
ColePhilips <cmp6593@psu.edu>
2025-01-30 16:57:06 -0500

    added express with passport now just need to actually setup passport

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport"); //Setup for passport to allow for cookies and sessions for the site
+const User = require('./models/User');
 
+const app = express();
+app.use(express.json());
+
+app.use(session({ 
+    secret: 'your_secret_key', 
+    resave: false, 
+    saveUninitialized: false
+  }));
+
+//Database setup
+main().catch(console.error);
 async function main() {
     await mongoose.connect("mongodb://localhost/Assignment3");
     console.log('Connected to MongoDB');
@@ -31,4 +46,30 @@ async function main() {
     model.exports = User;
 }
 
-main().catch(console.error);
+app.use(passport.initialize()); //initialize passport
+app.use(passport.session()); //use of passport
+
+app.post('/login', passport.authenticate('local'), (req, res) => {
+    res.send('Logged in successfully');
+})
+
+app.post('/register', async (req, res) => {
+    try{
+        const { username, password } = req.body;
+        const newUser = new User({ username, password });
+        await newUser.save();
+        res.send('User registered successfully');
+    } catch(error){
+        res.status(500).send(error.message);
+    }
+});
+
+app.get('/loggout', (req,res) => {
+    req.logout();
+    res.send('Logged out successfully');
+});
+
+app.get('/profile', (req, res) => {
+    if(!req.isAuthenticated()) return res.status(401).send('You are not authenticated');
+    res.send(`Hello ${req.use.username}`);
+});
